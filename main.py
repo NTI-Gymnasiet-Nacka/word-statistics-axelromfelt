1d767e65beb37b54ccd02e09e54d05638718556b
axelromfelt <114027887+axelromfelt@users.noreply.github.com>
2024-09-03 10:43:00 +0200

    e3rer

--- main.py
+++ main.py
@@ -6,9 +6,10 @@
 # - Genomsnittlig ordlängd
 # Gör en funktion för varje.
 
-# Bonus, gör en i taget, skapa en funktion för varje: 
+# Bonus, gör en i taget, skapa en funktion för varje:
 # - Längsta och kortaste ordet - om det finns flera, bestäm själv om du skriver ut ett eller flera
 # - Räkna antalet unika ord (alltså ord som bara förekommer en gång)
+LETTERS = "qwertyuiopasdfghjklzxcvbnmåöäQWERTYUIOPASDFGHJKLZXCVBNMÖÄÅ "
 
 
 def read_from_file(path: str):
@@ -20,12 +21,53 @@ def read_from_file(path: str):
     Returns:
         list(str): list of strings
     """
-    with open(path, "r" ,encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         return f.readlines()
 
+
+def count_words(text: list):
+    x = 0
+    text = "".join(text)
+    text = text.replace("\n", " ")
+    for character in text[::-1]:
+        if character not in LETTERS:
+            text = text.replace(character, "")
+    x += len(text.split())
+    return x
+
+
+def most_frequent(text: list):
+    text = "".join(text)
+    text = text.replace("\n", " ")
+    for character in text[::-1]:
+        if character not in LETTERS:
+            text = text.replace(character, "")
+    text = text.lower().split()
+    text.sort()
+    # dict = {}
+    # for word in text:
+    #     if word in dict:
+    #         dict[word] += 1
+    #     else:
+    #         dict[word] = 1
+    # print(dict)
+    # return max(dict, key=lambda word: dict[word])
+    old_count = 0
+    top_word = []
+    for word in text:
+        count = 0
+        for i in range(0, len(text)):
+            if text[i] == word:
+                count += 1
+
+
 def main():
-    
-    sentences = read_from_file("en_resa_genom_svenska_skogen.txt") # Här har du nu en lista av strängar från den inlästa filen.
+
+    # Här har du nu en lista av strängar från den inlästa filen.
+    sentences = read_from_file("en_resa_genom_svenska_skogen.txt")
+    print(count_words(sentences))
+    print(most_frequent(sentences))
+
 
 if __name__ == "__main__":
     main()
